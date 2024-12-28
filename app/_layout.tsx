a55d65efe9dcc8cbc0633cb2c388b11415d924ec
William Viana <vdawger@gmail.com>
2024-12-28 14:56:48 -0500

    header updates

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,4 @@
-import Ionicons from "@expo/vector-icons/Ionicons";
 import { Stack, useNavigation } from "expo-router";
-import { TouchableOpacity } from "react-native";
 
 export default function RootLayout() {
   const navigation = useNavigation();
@@ -8,23 +6,13 @@ export default function RootLayout() {
     <Stack
       screenOptions={{
         headerTitle: "Van Switches",
+        headerStyle: {
+          backgroundColor: "black",
+        },
+        headerTintColor: "white",
       }}
     >
-      <Stack.Screen
-        name="index"
-        options={({ navigation }) => ({
-          headerRight: () => (
-            <TouchableOpacity
-              onPress={(e) => {
-                console.error("Navigate to settings", e);
-                // navigation.navigate("settings");
-              }}
-            >
-              <Ionicons name="settings-outline" size={24} color="black" />
-            </TouchableOpacity>
-          ),
-        })}
-      />
+      <Stack.Screen name="index" options={({ navigation }) => ({})} />
       <Stack.Screen name="settings" options={{ title: "Settings" }} />
     </Stack>
   );
